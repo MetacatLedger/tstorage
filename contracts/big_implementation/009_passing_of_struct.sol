dd3fc54c61f7c5b604633a9bcae0cd204d8eaf48
androlo <androlo1980@gmail.com>
2018-12-09 21:49:15 +0100

    more examples

--- contracts/big_implementation/009_passing_of_struct.sol
+++ contracts/big_implementation/009_passing_of_struct.sol
@@ -16,28 +16,31 @@ contract Static {
 
 contract MsgReaderWriter is Static {
 
+    struct Info {
+        address addr;
+        bytes32 message;
+    }
+
     bytes32 constant internal TO_MSG = "Here's ur message.";
     bytes32 constant internal RE_MSG = "Thanks, bro.";
 
-	function msgRead() internal view returns (bytes32){
-		bytes32 msg;
+	function msgRead(address addr) internal view returns (Info memory) {
+		Info memory info;
         assembly {
-            msg := tload(address, 0x20)
+            mstore(info, tload(addr, 0x20))
+            mstore(add(info, 0x20), tload(addr, 0x40))
         }
-		return msg;
+		return info;
 	}
 
-	function msgRead(uint addr) internal pure returns (bytes32){
-		bytes32 msg;
-        assembly {
-            msg := tload(addr, 0x20)
-        }
-		return msg;
-	}
+    function msgRead() internal view returns (Info memory) {
+        return msgRead(address(this));
+    }
 
-	function msgWrite(bytes32 val) internal pure {
+	function msgWrite(Info memory info) internal pure {
         assembly {
-            tstore(0x20, val)
+            tstore(0x20, mload(info))
+            tstore(0x40, mload(add(info, 0x20)))
         }
 	}
 
@@ -50,16 +53,16 @@ contract Deployed is MsgReaderWriter {
     }
 
     function body() public view {
-        bytes32 msg = msgRead(uint160(msg.sender));
-		if (msg == TO_MSG) {
-			msgWrite(RE_MSG);
+        Info memory info = msgRead(msg.sender);
+		if (info.message == TO_MSG) {
+			msgWrite(Info(address(this), RE_MSG));
 		}
     }
 }
 
 contract Test is MsgReaderWriter {
 
-	bytes32 msg;
+	Info info;
 
     constructor() public {
         __STATIC_INIT();
@@ -69,8 +72,8 @@ contract Test is MsgReaderWriter {
         __STATIC_INIT();
 
         Deployed dep = new Deployed();
-        msgWrite(TO_MSG);
+        msgWrite(Info(address(this), TO_MSG));
 		dep.body();
-		msg = msgRead(uint160(address(dep)));
+		info = msgRead(address(dep));
     }
 }
